ff93db7617c6a2ee3a18cb6d233649c67622aacd
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-24 16:18:48 +0100

    start

---
+++ Corso-WD/vue-dc-comics/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+      <section>
+          <h1>ciao</h1>
+      </section>
+      <section>
+          
+      </section>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang='scss'>
+
+</style>
